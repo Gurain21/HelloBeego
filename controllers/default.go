22f2e5c98405535a97110567b4a844ca0e3070b7
zhouzhijie3607154 <1843607154@qq.com>
2020-09-25 12:05:33 +0800

    json格式多元化传值和软编码打开数据库

--- controllers/default.go
+++ controllers/default.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"HeeloBeego/models"
+	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego"
+	"io/ioutil"
 )
 
 type MainController struct {
@@ -10,10 +13,13 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+
+
 	c.Data["Website"] = "www.baidu.com"
 	c.Data["Email"] = "1843607154@qq.com"
 	c.TplName = "index.tpl"
-
+	//name1 := c.GetString("name")
+	//age1,err := c.GetInt("age")
 	name := c.Ctx.Input.Query("name")
 	age := c.Ctx.Input.Query("age")
 	sex := c.Ctx.Input.Query("sex")
@@ -23,7 +29,7 @@ func (c *MainController) Get() {
 //该post 方法时处理post类型的请求时要调用的方法
 func (c *MainController)Post() {
 	fmt.Println("post类型的请求")
-	// Ctx  *context.Context
+	// Ctx  *context.Context 环境
 	user := c.Ctx.Request.Form.Get("user")
 	pwd := c.Ctx.Request.Form.Get("pwd")
 	//老师的获取From中value的代码
@@ -37,10 +43,23 @@ func (c *MainController)Post() {
 
 	}
 
-
-
-
 	c.Data["Website"] = "www.baidu.com"
 	c.Data["Email"] = "1843607154@qq.com"
 	c.TplName = "index.tpl"
+
+	//body := c.Ctx.Request.Body
+	bytes,err := ioutil.ReadAll(c.Ctx.Request.Body)
+	if err != nil{
+		fmt.Println(err.Error())
+		c.Ctx.WriteString("数据接收失败")
+	}
+	var user1 models.Person
+	err = json.Unmarshal(bytes,&user1)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.Ctx.ResponseWriter.Write([]byte("数据解析失败"))
+	}
+	fmt.Println("用户名：",user1.Name)
+	fmt.Println("用户年龄：",user1.Age)
+	fmt.Println("用户性别：",user1.Sex)
 }
--- main.go
+++ main.go
@@ -2,10 +2,39 @@ package main
 
 import (
 	_ "HeeloBeego/routers"
+	"database/sql"
+	"fmt"
 	"github.com/astaxie/beego"
+	_ "github.com/go-sql-driver/msyql"
 )
 
 func main() {
-	beego.Run()
+	config := beego.AppConfig
+	appName :=config.String("appname")
+	fmt.Println("程序名称",appName)
+	httpPort,err := config.Int("httpport")
+	if err != nil {
+		panic("程序错误")
+
+	}
+	fmt.Println("监听端口",httpPort)
+	//beego.Run()
+	//"mysql", "root:123456@tcp(127.0.0.1:3306)/weather?charset=utf8"
+	db_dirver_name := config.String("db_dirver_name")
+	fmt.Println(db_dirver_name)
+	db_admin_name := config.String("db_admin_name")
+	db_admin_pwd := config.String("db_admin_pwd")
+	db_port := config.String("db_port")
+
+	db_name := config.String("db_name")
+	db,err := sql.Open(db_dirver_name,db_admin_name+":"+db_admin_pwd+"@tcp("+db_port+")/"+db_name+"?charset=utf8")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(db)
+	//"mysql", "root:123456@tcp(127.0.0.1:3306)/weather?charset=utf8"
+	//			root:123456@tcp(127.0.0.1:3306)/weather?charset=utf8
+
+	//fmt.Println(db_admin_name+":"+db_admin_pwd+"@tcp("+db_port+")/"+db_name+"?charset=utf8")
 }
 
